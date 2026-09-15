8f596de719d30edba4c05ae17492c5a4f6b88ea5
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-09-15 15:55:02 -0300

    chore:  remove nest starter controller and service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,15 +1,11 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 import { APP_PIPE } from '@nestjs/core';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { EnrollmentsModule } from './enrollments/enrollments.module.js';
 
 @Module({
   imports: [PrismaModule, EnrollmentsModule],
-  controllers: [AppController],
   providers: [
-    AppService,
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
